031cb664365fed6dfe54a372010d451acf9c9615
Aakash <aakashdinkarh@gmail.com>
2021-06-06 17:25:29 +0530

    final commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,42 @@
-// window.onload = () => {
-    function show_hide(a) {
-        let element = document.getElementById(a);
-        
-        if(this.innerHTML === 'Show more') {
-            $(element).show(500);
-            this.innerHTML = 'Show less';
-        } else {
-            $(element).hide(500);
-            this.innerHTML = 'Show more';
-        }
-    }
-    
+function go(loc) {
+  var elem = document.getElementById(loc);
+  elem.scrollIntoView();
+}
 
-    
-    function barclick() {
-        var baricon = document.getElementById("icon");
-        var closebtn = document.getElementById("close");
-        var navbar = document.getElementById('nav-bar');
-        navbar.style.width = '100%';
-        baricon.style.display = 'none';
-        closebtn.style.display = 'block';
-    }
-    function closeclick() {
-        var baricon = document.getElementById("icon");
-        var closebtn = document.getElementById("close");
-        var navbar = document.getElementById('nav-bar');
-        navbar.style.width = '0';
-        baricon.style.display = 'block';
-        closebtn.style.display = 'none';
-    }
+function show_hide(a) {
+  let element = document.getElementById(a);
 
-// }
+  if (this.innerHTML === "Show more") {
+    $(element).show(500);
+    this.innerHTML = "Show less";
+  } else {
+    $(element).hide(500);
+    this.innerHTML = "Show more";
+  }
+}
 
+window.onscroll = function () {
+  var topbtn = document.getElementById("top-btn");
+  if (document.documentElement.scrollTop < 71) {
+    topbtn.style.display = "none";
+  } else {
+    topbtn.style.display = "block";
+  }
+};
 
-
-
-    
+function barclick() {
+  var baricon = document.getElementById("icon");
+  var closebtn = document.getElementById("close");
+  var navbar = document.getElementById("nav-bar");
+  navbar.style.width = "100%";
+  baricon.style.display = "none";
+  closebtn.style.display = "block";
+}
+function closeclick() {
+  var baricon = document.getElementById("icon");
+  var closebtn = document.getElementById("close");
+  var navbar = document.getElementById("nav-bar");
+  navbar.style.width = "0";
+  baricon.style.display = "block";
+  closebtn.style.display = "none";
+}
